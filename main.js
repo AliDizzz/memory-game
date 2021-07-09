d7bae732193d65102fe2e89f113bcea2a53dbd58
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-07-09 11:00:52 +0200

    switchCase

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function afficherTableau() {
             if (tabJeu[i][j] === 0) {
                 txt += "<button class='btn btn-primary m-2' style='width:100px;height:100px;'>Afficher</button>";
             } else {
-                txt += "<img src='image/elephant.png' style='width:100px;height:100px' class='m-2'>";
+                txt += "<img src='"+getImage(tabJeu[i][j])+"' style='width:100px;height:100px' class='m-2'>";
             }
             
         }
@@ -26,4 +26,29 @@ function afficherTableau() {
     }
 
     divResultat.innerHTML = txt;
+}
+
+function getImage(valeur) {
+    var imgTxt = "image/";
+    switch (valeur) {
+        case 1: imgTxt += "elephant.png";
+            break;
+        case 2: imgTxt += "giraffe.png";
+            break;
+        case 3: imgTxt += "hippo.png";
+            break;
+        case 4: imgTxt += "monkey.png";
+            break;
+        case 5: imgTxt += "panda.png";
+            break;
+        case 6: imgTxt += "parrot.png";
+            break;
+        case 7: imgTxt += "penguin.png";
+            break;
+        case 8: imgTxt += "pig.png";
+            break;
+        default: console.log("cas non pris en compte");
+            break;
+    }
+    return 
 }
