cb85aa877d4d3686df9af2eba1df85bf8432e46c
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-07-15 09:51:23 +0200

    initP4

--- main2.js
+++ main2.js
@@ -4,15 +4,23 @@ var puissance4 = [];
 var nbColonne = 7;
 var nbLigne = 6;
 
-puissance4 = initialiserTableauVide(nbLigne, nbColonne);
-console.log(puissance4)
+puissance4 = initialiserTableauVide(nbLigne, nbColonne, 0);
+console.log(puissance4);
+
+/**
+ * Permet d'initialiser un tableau de tableau en fonction d'un nombre de ligne et de colonne passé en param
+ * @param {*} nbLigne 
+ * @param {*} nbColonne 
+ * @param {*} car 
+ * @returns 
+ */
 
 function initialiserTableauVide(nbLigne, nbColonne) {
     var tab = [];
     for (let i = 0; i < nbLigne; i++) {
         const ligne = [];
         for (let j = 0; j < nbColonne; j++) {
-            ligne.push(0);
+            ligne.push(car);
             
         }
         tab.push(ligne);
